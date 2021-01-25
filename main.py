ee454bd08ebc2e7f070b48c398f5343a672f6e13
Sean Doyle <sean@mendfamily.com>
2021-01-25 14:40:58 -0500

    pdf2image convert_from_path is loading the test pdf image.

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+from pdf2image import convert_from_path, convert_from_bytes
+
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+
+pdf_images = os.listdir('pdf_images')
+tiff_images = os.listdir('tiff_images')
+
+def convert_pdf_to_tiff(directory_path: str, img_name: str):
+    # pdf_images.save('test.Tiff', 'TIFF')
+    # images = convert_from_bytes(open(pdf_images, 'rb').read())
+    images = convert_from_path('./pdf_images/1.pdf')
+    print('pdf converted')
+    return images
+
+if __name__ == '__main__':
+    convert_pdf_to_tiff('pdf_image', '1.jpeg')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def convert_pdf_to_tiff(directory_path: str, img_name: str):
+#     # convert PDF to TIFF
+#     images_from_path = convert_from_path('/pdf_images', output_folder=tiff_images)
+#     # save tiff in tiff_images
+#     print('pdf converted')
+#
+# if __name__ == '__main__':
+#     convert_pdf_to_tiff('pdf_image', '1.jpeg')
