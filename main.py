99bf733702bbfd144a51e240ae06ab4eda394a84
Sean Doyle <sean@mendfamily.com>
2021-01-25 15:04:55 -0500

    Converts images from PDF to TIFF.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from pdf2image import convert_from_path, convert_from_bytes
-import tempfile
+
 
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
@@ -15,10 +15,10 @@ tiff_images = os.listdir('tiff_images')
 def convert_pdf_to_tiff():
     # pdf_images.save('test.Tiff', 'TIFF')
     # images = convert_from_bytes(open(pdf_images, 'rb').read())
-    with tempfile.TemporaryDirectory() as path:
-        images = convert_from_path('./pdf_images/1.pdf', output_folder='./tiff_images')
-        print('pdf converted')
-        return images
+    # with tempfile.TemporaryDirectory() as path:
+    images = convert_from_path('./pdf_images/1.pdf', output_folder='./tiff_images', fmt='tiff')
+    print(images)
+    return images
 
 if __name__ == '__main__':
     convert_pdf_to_tiff()
@@ -45,8 +45,18 @@ if __name__ == '__main__':
 
 
 
-
-
+# CONVERTS TO WRONG IMAGE TYPE
+# def convert_pdf_to_tiff():
+#     # pdf_images.save('test.Tiff', 'TIFF')
+#     # images = convert_from_bytes(open(pdf_images, 'rb').read())
+#     # with tempfile.TemporaryDirectory() as path:
+#     images = convert_from_path('./pdf_images/1.pdf', output_folder='./tiff_images')
+#     print('pdf converted')
+#     return images
+#
+# if __name__ == '__main__':
+#     convert_pdf_to_tiff()
+#
 
 
 
